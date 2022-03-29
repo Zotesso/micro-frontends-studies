b2e0c77d6cc13c1a0013a70b51fa0d2a88399616
Pedro Zotesso <siriothart@gmail.com>
2022-03-28 22:15:54 -0300

    Started 2 applications | Host | Remote

---
+++ remote/src/App.tsx
@@ -0,0 +1,13 @@
+import { render } from "solid-js/web";
+
+import Counter from "./Counter";
+
+import "./index.css";
+
+const App = () => (
+  <div class="container">
+    <div>Name: remote</div>
+    <Counter />
+  </div>
+);
+render(App, document.getElementById("app"));
